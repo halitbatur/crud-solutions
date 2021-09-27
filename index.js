1630cac9397fa72aa539361af92c381511b8e130
osamaakb <osama98akb@gmail.com>
2021-09-27 15:38:56 +0300

    fix blogPostController

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(postsController);
+app.use(blogPostsController);
 
 const port = 3000;
 app.listen(port, () => {
